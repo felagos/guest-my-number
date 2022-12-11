5356b499ba50f1dce0f3f5f1487430088a1757ce
Felipe Lagos <felagossanchez@gmail.com>
2022-12-11 20:10:23 -0300

    add screens

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { StartGameScreen } from './src/screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+			<StartGameScreen />
     </View>
   );
 }
@@ -13,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
